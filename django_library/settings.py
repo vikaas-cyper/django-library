073f99c18141bac4e3ec853a1ff723fd2ef2eb39
vikaas-a <vikaas.cs20@bitsathy.ac.in>
2023-03-04 20:54:40 +0530

    database and library app created

--- django_library/settings.py
+++ django_library/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'django_library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_library',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'projects',
+        'PASSWORD': 'project'
     }
 }
 
